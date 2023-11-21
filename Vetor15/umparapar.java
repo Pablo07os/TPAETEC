68718a40d2b4403707a55de4183e778e3371b68c
Pablo07os <143467378+Pablo07os@users.noreply.github.com>
2023-11-21 11:31:08 -0300

    Update umparapar.java

--- Vetor15/umparapar.java
+++ Vetor15/umparapar.java
@@ -1,28 +1,24 @@
 package vetores;
 import java.util.Scanner;
-public class umparapar {
-	public static void main(String[]args) {
-		
-		Scanner leia = new Scanner (System.in);
-		final int TAM=10;
-		int a[], i, b[];
-		a = new int [TAM];
-		b = new int [TAM];
-		
-		for (i=0; i<TAM; i++) {
-			System.out.println("Digite o "+(i+1)+"termo do veotr a");
-			a[i] = leia.nextInt();
-			
-		if (a[i] % 2 ==0) {
-			b[i] = 1;
-		}else {
-			b[i] = 0;
-		}
-		}
-		for(i=0; i<TAM; i++) {
-		System.out.println(+b[i]);
-	}
-	}
-}
+public class tabuada {
+	   public static void main(String[] args) {
+	        Scanner leia = new Scanner(System.in);
+	        final int TAM = 5;
+	        int a[], b[], r;
 
+	        a = new int[TAM];
+	        b = new int[TAM];
 
+	        for (int i = 0; i < TAM; i++) {
+	            System.out.println("Digite o " + (i + 1) + "º termo do vetor a");
+	            a[i] = leia.nextInt();
+	        }
+
+	        for (int i = 0; i < TAM; i++) {
+	            for (int j = 1; j <= 10; j++) {
+	                r = a[i] * j;
+	                System.out.println(a[i] + " x " + j + " = " + r);
+	            }
+	        }
+	    }
+	}
